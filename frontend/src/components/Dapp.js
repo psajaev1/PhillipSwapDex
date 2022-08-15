0b42aeda3f63c9ccad40a7d87d5a3547f623ce46
psajaev1 <psajaev@gmail.com>
2022-08-14 20:17:07 -0500

    Add test network configuration

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -191,9 +191,9 @@ export class Dapp extends React.Component {
     // Once we have the address, we can initialize the application.
 
     // First we check the network
-    if (!this._checkNetwork()) {
-      return;
-    }
+    // if (!this._checkNetwork()) {
+    //   return;
+    // }
 
     this._initialize(selectedAddress);
 
@@ -345,23 +345,18 @@ export class Dapp extends React.Component {
   }
 
   // This method checks if Metamask selected network is Localhost:8545 
-  _checkNetwork() {
-    if (window.ethereum.networkVersion === HARDHAT_NETWORK_ID) {
-      return true;
-    }
+  // _checkNetwork() {
+  //   if (window.ethereum.networkVersion === HARDHAT_NETWORK_ID) {
+  //     return true;
+  //   }
 
-    this.setState({ 
-      networkError: 'Please connect Metamask to Localhost:8545'
-    });
+  //   this.setState({ 
+  //     networkError: 'Please connect Metamask to Localhost:8545'
+  //   });
 
-    return false;
-  }
+  //   return false;
+  // }
 
-
-  // start working on adding liquidity 
-  // Then on actually swapping, then make UI look nice 
-  // Then prob implement spinner 
-  // Then impelement making more pairs and making it scalable
 
   async _createPairing() {
     try {
@@ -393,63 +388,5 @@ export class Dapp extends React.Component {
     
   }
 
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // async _transferTokens(to, amount) {
-  //   // Sending a transaction is a complex operation:
-  //   //   - The user can reject it
-  //   //   - It can fail before reaching the ethereum network (i.e. if the user
-  //   //     doesn't have ETH for paying for the tx's gas)
-  //   //   - It has to be mined, so it isn't immediately confirmed.
-  //   //     Note that some testing networks, like Hardhat Network, do mine
-  //   //     transactions immediately, but your dapp should be prepared for
-  //   //     other networks.
-  //   //   - It can fail once mined.
-  //   //
-  //   // This method handles all of those things, so keep reading to learn how to
-  //   // do it.
-
-  //   try {
-  //     // If a transaction fails, we save that error in the component's state.
-  //     // We only save one such error, so before sending a second transaction, we
-  //     // clear it.
-  //     this._dismissTransactionError();
-
-  //     // We send the transaction, and save its hash in the Dapp's state. This
-  //     // way we can indicate that we are waiting for it to be mined.
-  //     const tx = await this._token.transfer(to, amount);
-  //     this.setState({ txBeingSent: tx.hash });
-
-  //     // We use .wait() to wait for the transaction to be mined. This method
-  //     // returns the transaction's receipt.
-  //     const receipt = await tx.wait();
-
-  //     // The receipt, contains a status flag, which is 0 to indicate an error.
-  //     if (receipt.status === 0) {
-  //       // We can't know the exact error that made the transaction fail when it
-  //       // was mined, so we throw this generic one.
-  //       throw new Error("Transaction failed");
-  //     }
-
-  //     // If we got here, the transaction was successful, so you may want to
-  //     // update your state. Here, we update the user's balance.
-  //     await this._updateBalance();
-  //   } catch (error) {
-  //     // We check the error code to see if this error was produced because the
-  //     // user rejected a tx. If that's the case, we do nothing.
-  //     if (error.code === ERROR_CODE_TX_REJECTED_BY_USER) {
-  //       return;
-  //     }
-
-  //     // Other errors are logged and stored in the Dapp's state. This is used to
-  //     // show them to the user, and for debugging.
-  //     console.error(error);
-  //     this.setState({ transactionError: error });
-  //   } finally {
-  //     // If we leave the try/catch, we aren't sending a tx anymore, so we clear
-  //     // this part of the state.
-  //     this.setState({ txBeingSent: undefined });
-  //   }
-  // }
 
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,12 +5,21 @@ require("@nomicfoundation/hardhat-toolbox");
 // testing the frontend.
 require("./tasks/faucet");
 
+const ALCHEMY_API_KEY = "RWfYADQdLjY9J4TBwFdul5ljm7-toiiD";
+
+const GOERLI_PRIVATE_KEY = "9919170bdfb0d2909b644ccefcf6b10b9a58f6f373c6e58415b991049d5c0f30";
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
-    }
+    }, 
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    },
   }
 };
